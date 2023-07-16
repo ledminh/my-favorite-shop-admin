ed54c65298a421a5a7b4e769a2604993dd6565eb
Minh Le <minh.le@ledminh.dev>
2023-07-16 13:10:00 -0400

    Redesign MessageTab

--- src/components/MessageTab.tsx
+++ src/components/MessageTab.tsx
@@ -31,15 +31,21 @@ type ButtonProps = {
 };
 
 const Button = ({ item, onClick }: ButtonProps) => {
-  const { firstName, lastName, email, phone, status } = item;
+  const { firstName, lastName, email, phone, status, message } = item;
 
+  const _message = message.length > 50 ? message.slice(0, 50) + "..." : message;
+
+  const Status =
+    status === "read" ? null : (
+      <span className="px-2 text-sm text-white bg-red-800">{status}</span>
+    );
   return (
     <button
       className="flex flex-col w-full gap-2 p-2 rounded-lg hover:ring hover:ring-blue-900 active:bg-orange-200"
       onClick={onClick}
     >
       <div className="flex flex-row justify-between">
-        <div className="flex flex-col">
+        <div className="flex flex-col items-start">
           <span className="text-sm font-medium text-gray-900">
             {firstName} {lastName}
           </span>
@@ -53,7 +59,10 @@ const Button = ({ item, onClick }: ButtonProps) => {
         <div className="flex flex-col">
           <span className="text-sm text-gray-500">{phone}</span>
         </div>
-        <span className="text-sm text-gray-500">{status}</span>
+        {Status}
+      </div>
+      <div className="flex flex-col items-start">
+        <span className="text-sm text-left text-gray-500">{_message}</span>
       </div>
     </button>
   );
