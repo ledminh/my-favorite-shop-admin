5fbe3b0e68b4a9d4ad36a6ef5dd1a7bc9ae12b19
Minh Le <minh.le@ledminh.dev>
2023-07-15 21:36:24 -0400

    Sort

--- src/components/Sorts.tsx
+++ src/components/Sorts.tsx
@@ -1,24 +1,112 @@
 "use client";
 
 import Select from "@/components/layout/Select";
+import { useEffect, useState } from "react";
 
-export default function CategoryDropdown() {
-  const categories = [
-    { id: "1", text: "Category 1" },
-    { id: "2", text: "Category 2" },
-    { id: "3", text: "Category 3" },
-    { id: "4", text: "Category 4" },
-    { id: "5", text: "Category 5" },
-    { id: "6", text: "Category 6" },
-    { id: "7", text: "Category 7" },
-    { id: "8", text: "Category 8" },
-  ];
+const sortByOptions = [
+  {
+    id: "name",
+    text: "Name",
+    orderOptions: [
+      {
+        id: "asc",
+        text: "A to Z",
+      },
+      {
+        id: "desc",
+        text: "Z to A",
+      },
+    ],
+  },
+  {
+    id: "price",
+    text: "Price",
+    orderOptions: [
+      {
+        id: "asc",
+        text: "Low to High",
+      },
+      {
+        id: "desc",
+        text: "High to Low",
+      },
+    ],
+  },
+  {
+    id: "create-at",
+    text: "Create At",
+    orderOptions: [
+      {
+        id: "asc",
+        text: "Oldest to Newest",
+      },
+      {
+        id: "desc",
+        text: "Newest to Oldest",
+      },
+    ],
+  },
+  {
+    id: "modified-at",
+    text: "Modified At",
+    orderOptions: [
+      {
+        id: "asc",
+        text: "Oldest to Newest",
+      },
+      {
+        id: "desc",
+        text: "Newest to Oldest",
+      },
+    ],
+  },
+];
 
-  const options = [{ id: "all", text: "All" }, ...categories];
+export default function Sorts() {
+  const [sortByID, setSortByID] = useState(sortByOptions[0].id);
 
-  const onChange = (id: string) => {
-    console.log(id);
-  };
+  const [orderOptions, setOrderOptions] = useState(
+    sortByOptions[0].orderOptions
+  );
 
-  return <Select options={options} defaultValue="all" onChange={onChange} />;
+  const [orderID, setOrderID] = useState(orderOptions[0].id);
+
+  const sortByOnChange = (id: string) => setSortByID(id);
+
+  const orderOnChange = (id: string) => setOrderID(id);
+
+  useEffect(() => {
+    const orderOptions = sortByOptions.find((option) => option.id === sortByID);
+
+    if (orderOptions) {
+      setOrderOptions(orderOptions.orderOptions);
+    }
+  }, [sortByID]);
+
+  useEffect(() => {
+    console.log(sortByID, orderID);
+  }, [sortByID, orderID]);
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="sortBy">Sort by</label>
+        <Select
+          id="sortBy"
+          options={sortByOptions}
+          defaultValue={sortByOptions[0].id}
+          onChange={sortByOnChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="order">Order</label>
+        <Select
+          id="order"
+          options={orderOptions}
+          defaultValue={orderOptions[0].id}
+          onChange={orderOnChange}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/layout/Select.tsx
+++ src/components/layout/Select.tsx
@@ -1,12 +1,14 @@
 type Props = {
+  id: string;
   options: { id: string; text: string }[];
   defaultValue: string;
   onChange: (id: string) => void;
 };
 
-const Select = ({ options, defaultValue, onChange }: Props) => {
+const Select = ({ id, options, defaultValue, onChange }: Props) => {
   return (
     <select
+      id={id}
       defaultValue={defaultValue}
       className="block p-2 text-sm text-gray-900 border-0 rounded-md basis-full ring-1 ring-gray-600 hover:text-gray-900 focus:outline-none focus:ring-gray-900 focus:ring-offset-2 focus:ring-offset-gray-100"
       onChange={(e) => onChange(e.target.value)}
