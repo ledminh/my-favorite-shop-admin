793f38c2de4e3e229a7432c7c46fb75f636b20e5
Minh Le <minh.le@ledminh.dev>
2023-07-18 20:36:07 -0400

    Add ControlPanel to messages page

--- src/app/(content_page)/messages/page.tsx
+++ src/app/(content_page)/messages/page.tsx
@@ -2,6 +2,7 @@ import { getCustomerMessages } from "@/data/customerMessages";
 
 import MessageList from "@/components/messages/MessageList";
 import { itemsPerPage } from "@/config";
+import ControlPanel from "@/components/messages/ControlPanel";
 
 export default async function MessagesPage() {
   const { items, total } = await getCustomerMessages({
@@ -12,8 +13,9 @@ export default async function MessagesPage() {
   });
 
   return (
-    <>
+    <div className="m-4">
+      <ControlPanel />
       <MessageList initMessages={items} total={total} />
-    </>
+    </div>
   );
 }
