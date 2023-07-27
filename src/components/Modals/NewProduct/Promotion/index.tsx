14dc172a45497bb611000554ff5a25af854d7986
Minh Le <minh.le@ledminh.dev>
2023-07-27 18:37:34 -0400

    Add Promotion to AddNewProd modal

--- src/components/Modals/NewProduct/Promotion/index.tsx
+++ src/components/Modals/NewProduct/Promotion/index.tsx
@@ -5,25 +5,28 @@ import { CheckCircleIcon } from "@heroicons/react/20/solid";
 
 import { useState } from "react";
 
+import ToggleButton from "./ToggleButton";
+
 export default function Promotion() {
-  const [selectedMailingLists, setSelectedMailingLists] = useState(
-    mailingLists[0]
-  );
+  const [selectedPromotion, setSelectedPromotion] = useState(promotionList[0]);
   return (
-    <RadioGroup value={selectedMailingLists} onChange={setSelectedMailingLists}>
-      <RadioGroup.Label className="text-base font-semibold leading-6 text-gray-900">
-        Select a mailing list
+    <RadioGroup value={selectedPromotion} onChange={setSelectedPromotion}>
+      <RadioGroup.Label className="flex gap-2 text-base leading-6 text-gray-900">
+        <span>Promotion</span>
+        <div>
+          <ToggleButton />
+        </div>
       </RadioGroup.Label>
 
       <div className="grid grid-cols-1 mt-4 gap-y-6 sm:grid-cols-3 sm:gap-x-4">
-        {mailingLists.map((mailingList) => (
+        {promotionList.map((promotion) => (
           <RadioGroup.Option
-            key={mailingList.id}
-            value={mailingList}
+            key={promotion.id}
+            value={promotion}
             className={({ active }) =>
               classNames(
                 active
-                  ? "border-indigo-600 ring-2 ring-indigo-600"
+                  ? "border-blue-950 ring-2 ring-blue-950"
                   : "border-gray-300",
                 "relative flex cursor-pointer rounded-lg border bg-white p-4 shadow-sm focus:outline-none"
               )
@@ -32,38 +35,39 @@ export default function Promotion() {
             {({ checked, active }) => (
               <>
                 <span className="flex flex-1">
-                  <span className="flex flex-col">
+                  <span className="flex flex-col gap-4">
                     <RadioGroup.Label
                       as="span"
                       className="block text-sm font-medium text-gray-900"
                     >
-                      {mailingList.title}
+                      {promotion.title}
                     </RadioGroup.Label>
                     <RadioGroup.Description
                       as="span"
                       className="flex items-center mt-1 text-sm text-gray-500"
                     >
-                      {mailingList.description}
-                    </RadioGroup.Description>
-                    <RadioGroup.Description
-                      as="span"
-                      className="mt-6 text-sm font-medium text-gray-900"
-                    >
-                      {mailingList.users}
+                      <input
+                        className="border border-gray-300 rounded-md"
+                        type="number"
+                        placeholder="0"
+                        min="0"
+                        max="100"
+                        step="1"
+                      />
                     </RadioGroup.Description>
                   </span>
                 </span>
                 <CheckCircleIcon
                   className={classNames(
                     !checked ? "invisible" : "",
-                    "h-5 w-5 text-indigo-600"
+                    "h-5 w-5 text-blue-950"
                   )}
                   aria-hidden="true"
                 />
                 <span
                   className={classNames(
                     active ? "border" : "border-2",
-                    checked ? "border-indigo-600" : "border-transparent",
+                    checked ? "border-blue-950" : "border-transparent",
                     "pointer-events-none absolute -inset-px rounded-lg"
                   )}
                   aria-hidden="true"
@@ -88,23 +92,17 @@ function classNames(...classes: string[]) {
  * Data
  */
 
-const mailingLists = [
+const promotionList = [
   {
-    id: 1,
-    title: "Newsletter",
+    id: "discount",
+    title: "Discount",
     description: "Last message sent an hour ago",
     users: "621 users",
   },
   {
-    id: 2,
-    title: "Existing Customers",
+    id: "sale",
+    title: "Sale",
     description: "Last message sent 2 weeks ago",
     users: "1200 users",
-  },
-  {
-    id: 3,
-    title: "Trial Users",
-    description: "Last message sent 4 days ago",
-    users: "2740 users",
   },
 ];
