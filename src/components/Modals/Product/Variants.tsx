c1a400060262310875385542b85cb8d05d0a8c63
Minh Le <minh.le@ledminh.dev>
2023-08-03 17:57:55 -0400

    Factor out Variants from ProductModal

---
+++ src/components/Modals/Product/Variants.tsx
@@ -0,0 +1,24 @@
+export default function Variants() {
+  return (
+    <>
+      <span>Variant</span>
+      <ul>
+        <li>
+          <button>Add new variant</button>
+        </li>
+        <li>
+          <button>Variant 1</button>
+        </li>
+        <li>
+          <button>Variant 2</button>
+        </li>
+        <li>
+          <button>Variant 3</button>
+        </li>
+        <li>
+          <button>Variant 4</button>
+        </li>
+      </ul>
+    </>
+  );
+}
