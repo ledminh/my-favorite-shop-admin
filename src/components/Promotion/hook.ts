466c780b5b19e9c6d44d705e1108c97f3985db04
Minh Le <minh.le@ledminh.dev>
2023-08-19 23:17:22 -0400

    Edit Variant

--- src/components/Promotion/hook.ts
+++ src/components/Promotion/hook.ts
@@ -5,44 +5,18 @@ import { Props, _PromotionType } from "./types";
 export default function usePromotion(props: Props) {
   const { onChange, initPromotion } = props;
 
-  const initSelectedPromotion = initPromotion
-    ? (promotionList.find(
-        (promotion) => promotion.id === initPromotion.type
-      ) as _PromotionType)
-    : null;
-
-  const initDiscountPercentStr =
-    initPromotion && initPromotion.type === "discount"
-      ? initPromotion.discountPercent.toString()
-      : "0";
-  const initSalePriceStr =
-    initPromotion && initPromotion.type === "sale"
-      ? initPromotion.salePrice.toString()
-      : "0";
-
-  const initDiscountDescription =
-    initPromotion && initPromotion.type === "discount"
-      ? initPromotion.description
-      : "";
-  const initSaleDescription =
-    initPromotion && initPromotion.type === "sale"
-      ? initPromotion.description
-      : "";
+  const [initiated, setInitiated] = useState(false);
 
   const [enabled, setEnabled] = useState(false);
 
   const [selectedPromotion, setSelectedPromotion] =
-    useState<_PromotionType | null>(initSelectedPromotion);
+    useState<_PromotionType | null>(null);
 
-  const [discountPercentStr, setDiscountPercentStr] = useState(
-    initDiscountPercentStr
-  );
-  const [salePriceStr, setSalePriceStr] = useState(initSalePriceStr);
+  const [discountPercentStr, setDiscountPercentStr] = useState("0");
+  const [salePriceStr, setSalePriceStr] = useState("0");
 
-  const [discountDescription, setDiscountDescription] = useState(
-    initDiscountDescription
-  );
-  const [saleDescription, setSaleDescription] = useState(initSaleDescription);
+  const [discountDescription, setDiscountDescription] = useState("");
+  const [saleDescription, setSaleDescription] = useState("");
 
   const resetDiscount = () => {
     setDiscountPercentStr("0");
@@ -55,13 +29,82 @@ export default function usePromotion(props: Props) {
   };
 
   useEffect(() => {
+    if (initPromotion) {
+      setEnabled(true);
+    } else {
+      setEnabled(false);
+    }
+
+    const initSelectedPromotion = initPromotion
+      ? (promotionList.find(
+          (promotion) => promotion.id === initPromotion.type
+        ) as _PromotionType)
+      : null;
+
+    const initDiscountPercentStr =
+      initPromotion && initPromotion.type === "discount"
+        ? initPromotion.discountPercent.toString()
+        : "0";
+    const initSalePriceStr =
+      initPromotion && initPromotion.type === "sale"
+        ? initPromotion.salePrice.toString()
+        : "0";
+
+    const initDiscountDescription =
+      initPromotion && initPromotion.type === "discount"
+        ? initPromotion.description
+        : "";
+
+    const initSaleDescription =
+      initPromotion && initPromotion.type === "sale"
+        ? initPromotion.description
+        : "";
+
+    setSelectedPromotion(initSelectedPromotion);
+    setDiscountPercentStr(initDiscountPercentStr);
+    setSalePriceStr(initSalePriceStr);
+    setDiscountDescription(initDiscountDescription);
+    setSaleDescription(initSaleDescription);
+
+    setInitiated(true);
+  }, [initPromotion]);
+
+  useEffect(() => {
+    if (!initiated) return;
+
     if (!enabled) {
-      setSelectedPromotion(null);
-      onChange(null);
       resetDiscount();
       resetSale();
+
+      setSelectedPromotion(null);
+      onChange(null);
     } else {
-      setSelectedPromotion(promotionList[0]);
+      /**
+       * Where does it come from?
+       * 1. user clicks on the promotion toggle when enabled === false
+       *    - initPromotion is null/undefined, null when there is a product with no promotion, undefined when there is no product --> also no promotion --> selectedPromotion =  promotionList[0]
+       *    - initPromotion is not null/undefined --> there is an initProduct with promotion but user already deleted it --> still selectedPromotion =  promotionList[0]
+       *
+       * 2. first time loading the modal and initPromotion is not null
+       *  - initPromotion is not null/undefined --> there is an initProduct with promotion --> selectedPromotion = listPromotion.find(promotion => promotion.id === initPromotion.type), set description, set discountPercentStr/salePriceStr
+       *
+       *  how to tell if it is the first case or the second case?
+       *  - if initPromotion is null/undefined --> first case
+       * - if initPromotion is not null/undefined --> second case
+       *   can't be, because initPromotion is either null or undefined in both cases.
+       *  --> so we can't tell if it is the first case or the second case. So how?
+       *
+       *
+       */
+
+      if (!initPromotion) setSelectedPromotion(promotionList[0]);
+      else {
+        const initSelectedPromotion = promotionList.find(
+          (promotion) => promotion.id === initPromotion.type
+        ) as _PromotionType;
+
+        setSelectedPromotion(initSelectedPromotion);
+      }
     }
   }, [enabled]);
 
