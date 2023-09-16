d92810984be9c900b4bf45b335f3c7e6d3bc80f7
Minh Le <minh.le@ledminh.dev>
2023-09-16 16:22:37 -0400

    NewCatButton

--- src/components/home/Button.tsx
+++ src/components/home/Button.tsx
@@ -1,11 +1,12 @@
 type Props = {
   children: React.ReactNode;
   onClick?: () => void;
+  classNames?: string;
 };
 
-const Button = ({ children, onClick }: Props) => (
+const Button = ({ children, onClick, classNames }: Props) => (
   <button
-    className="p-4 font-bold border-2 rounded-lg border-blue-950 text-blue-950 hover:bg-blue-950/10 hover:shadow-stone-800 hover:shadow-md active:bg-blue-950/20"
+    className={`p-4 font-bold border-2 rounded-lg border-blue-950 text-blue-950 hover:bg-blue-950/10 hover:shadow-stone-800 hover:shadow-md active:bg-blue-950/20 ${classNames}`}
     onClick={onClick}
   >
     {children}
